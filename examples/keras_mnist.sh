27abdd7809ef1f8a7a35e3f539d6f1193c509bf1
brettin <brettin@anl.gov>
2017-10-06 15:34:09 +0000

    renamed

--- examples/keras_mnist.sh
+++ examples/keras_mnist.sh
@@ -1,9 +1,12 @@
-
 #!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR
 
 function_to_fork() {
   top -b -n 60 -d 60 -u brettin > $COBALT_JOBID.$$.top
 }
 function_to_fork &
 
-python keras_minst.py
+
+python $DIR/keras_mnist.py
