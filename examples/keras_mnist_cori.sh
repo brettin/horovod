8325c3c18fb0031d71446c89b7f5a7ace08d63e0
brettin <brettin@anl.gov>
2017-11-06 13:00:30 -0800

    added more env stuff

--- examples/keras_mnist_cori.sh
+++ examples/keras_mnist_cori.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
+#SBATCH -N 2
+#SBATCH -p debug
+#SBATCH -C knl,quad,cache
+#SBATCH -t 30:00
+#SBATCH -L SCRATCH
+
+module load python/2.7-anaconda
+module load darshan
+
+export LD_LIBRARY_PATH=/usr/common/software/darshan/3.1.4/lib:$LD_LIBRARY_PATH
+source activate horovod-tf2
+
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
-
-function_to_fork() {
-  top -b -n 60 -d 60 -u brettin > $SLURM_JOBID.$$.top
-}
-function_to_fork &
 
 export OMP_NUM_THREADS=136
 export KMP_BLOCKTIME=30
 export KMP_SETTINGS=1
 export KMP_AFFINITY="granularity=fine,verbose,compact,1,0"
 
+srun python $DIR/keras_mnist.py
 
-python $DIR/keras_mnist.py
+source deactivate
